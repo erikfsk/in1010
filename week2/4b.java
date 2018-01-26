4fc039f202ce40a6f7c5359ff022c1bc4dd7d4d4
Unknown <erikfsk@student.matnat.uio.no>
2018-01-26 13:46:42 +0100

    Finn feil oppdatert

--- week2/4b.java
+++ week2/4b.java
@@ -4,7 +4,7 @@ class Test_4b{
 		String b = "4";
 		double c = 10.2;
 		System.out.println(a + 5);
-		// System.out.println(a + b);
+		System.out.println(a + b);
 		// int sum = a + b;
 		// System.out.println(sum);
 		// int sum = a + c;
