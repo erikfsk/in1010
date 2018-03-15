9f58491c85348ce098ec7bcfc7143397121cf566
Unknown <erikfsk@student.matnat.uio.no>
2018-03-16 00:08:03 +0100

    Uke 9

---
+++ week9/BinaryTree.java
@@ -0,0 +1,48 @@
+class Test{
+    public static void main(String[] args) {
+        BinaryTree testTre = new BinaryTree(10);
+        testTre.settInn(7);
+        testTre.settInn(17);
+        testTre.settInn(5);
+        testTre.settInn(15);
+        testTre.settInn(8);
+        testTre.settInn(18);
+        testTre.skrivUt();//skriv ut alle verdiene, i synkende rekkefølge
+        System.out.println(fakultet(0));
+        System.out.println(fakultet(1));
+        System.out.println(fakultet(2));
+        System.out.println(fakultet(3));
+        System.out.println(fakultet(4));
+        System.out.println(fakultet(10));
+    }
+
+    public static int fakultet(int i){
+
+    }
+}
+
+class BinaryTree{
+    private Node head;
+
+    BinaryTree(int input){
+
+    }
+    private class Node{
+
+    }
+
+    public boolean settInn(int input){
+
+    }
+
+    public boolean settInn(Node currentNode,int input){
+
+    }
+
+    public void skrivUt(){
+
+    }
+
+    public void skrivUt(Node currentNode){
+    }
+}
