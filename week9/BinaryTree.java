1676c9ec9a8d2371ec6fb72e6b89662baa6eec95
Unknown <erikfsk@student.matnat.uio.no>
2018-03-16 14:00:02 +0100

    God helg :)

--- week9/BinaryTree.java
+++ week9/BinaryTree.java
@@ -8,16 +8,21 @@ class Test{
         testTre.settInn(8);
         testTre.settInn(18);
         testTre.skrivUt();//skriv ut alle verdiene, i synkende rekkefølge
-        System.out.println(fakultet(0));
-        System.out.println(fakultet(1));
-        System.out.println(fakultet(2));
-        System.out.println(fakultet(3));
-        System.out.println(fakultet(4));
-        System.out.println(fakultet(10));
+        // System.out.println(fakultet(0));
+        // System.out.println(fakultet(1));
+        // System.out.println(fakultet(2));
+        // System.out.println(fakultet(3));
+        // System.out.println(fakultet(4));
+        // System.out.println(fakultet(10));
     }
 
     public static int fakultet(int i){
-
+        if (i < 0){
+            return 0;
+        }
+        if (i == 0) {
+            return 1;
+        } return fakultet(i-1)*i;
     }
 }
 
@@ -25,24 +30,56 @@ class BinaryTree{
     private Node head;
 
     BinaryTree(int input){
-
+        head = new Node(input);
     }
     private class Node{
+        Node right;
+        Node left;
+        int value;
 
+        Node(int input){
+            this.value = input;
+        }
     }
 
     public boolean settInn(int input){
-
+         return settInn(head,input);
     }
 
     public boolean settInn(Node currentNode,int input){
-
+        if (currentNode.value > input) {
+            if (currentNode.left != null){
+                return settInn(currentNode.left,input);
+            }
+            currentNode.left = new Node(input);
+            return true;
+        } else if (currentNode.value < input){
+            if (currentNode.right != null) {
+                return settInn(currentNode.right,input);
+            }
+            currentNode.right = new Node(input);
+            return true;
+        } return false;
     }
 
     public void skrivUt(){
-
+        skrivUt(head);
     }
 
     public void skrivUt(Node currentNode){
+        if (currentNode.right != null){
+            skrivUt(currentNode.right);
+        }System.out.println(currentNode.value);
+        if (currentNode.left != null){
+            skrivUt(currentNode.left);
+        }
     }
 }
+
+
+// if (currentNode.right != null){
+//     System.out.println(currentNode.value);
+// }System.out.println(currentNode.value);
+// if (currentNode.left != null){
+//     skrivUt(currentNode.left);
+// }
